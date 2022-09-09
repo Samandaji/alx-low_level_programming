5167e125339cdbe5fb7a9aa7ef4b7c758436da90
Abubakar Usman <contactabubakarusman@gmail.com>
2022-09-09 07:40:05 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,9 +6,10 @@
  * Return: Always 0 (success)
  *
  */
-int main(void)
+
+   int main(void)
+
  {
     puts("\"programming is like building a multilingual puzzle");
-	       
-	       return(0);
+	    return (0);
  }
