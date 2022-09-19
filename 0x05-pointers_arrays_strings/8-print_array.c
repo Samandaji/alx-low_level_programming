b3c6736bb9fb806a47ec0530060b80db7bc43f8e
Abubakar Usman <contactabubakarusman@gmail.com>
2022-09-19 13:31:50 -0700

    8-print_array.c

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,13 +10,13 @@ void print_array(int *a, int n)
 	int i;
 
 	i = 0;
-	for (n--; n >= 0; n--, i++)
+	for (; i < n; i++)
 	{
 		printf("%d", a[i]);
-		if (n > 0)
+		if (i < n-1)
 		{
 			printf(" , ");
 		}
 	}
-	printf("\n");
+           putchar("\n");
 }
