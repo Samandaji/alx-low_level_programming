bda2853dd0025c7f0efd684b7583b0b421024b50
Samandaji <contactabubakarusman@gmail.com>
2022-09-24 16:57:13 +0100

    Revert "3-strcmp.c"
    This reverts commit 5daff9b6f6ba1824e1ca576ecd8df0de14c4abbf.

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,16 +1,16 @@
 #include "main.h"
-#include <string.h>
-#include <stdio.h>
 /**
  * _strcmp - function to copy string
  * @s1: string 1
  * @s2: string 2
  * Return: int
- *
  */
 int _strcmp(char *s1, char *s2)
 {
-int i = strcmp(s1, s2);
-
-	return (i);
+	while (*s1 == *s2)
+	{
+		++s1;
+		++s2;
+	}
+	return ((int)(unsigned char)(*s1) - (int)(unsigned char)(*s2));
 }
