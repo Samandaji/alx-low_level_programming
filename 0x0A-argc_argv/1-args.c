8b13265b5b36cc5a10c5d4d37b6edb75f60b871a
Abubakar Usman <contactabubakarusman@gmail.com>
2022-10-03 09:34:27 -0700

    1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#define UNUSED(x) (void)(x)
+/**
+ * main - main function
+ * @argc: argument c
+ * @argv: vector arguments
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	UNUSED(argv);
+	printf("%d\n", argc - 1);
+	return (0);
+}
