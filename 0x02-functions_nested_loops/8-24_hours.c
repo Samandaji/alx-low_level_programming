047cd2a2c11da7477ca4d8a9a95c062ac155b2b8
samandaji <contactabubakarusman@gmail.com>
2022-12-15 11:42:20 +0100

    updated 8-24

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,40 +1,32 @@
-#include"main.h"
+#include "main.h"
 /**
- * jack_bauer - prints every minute of the of jack Bauer
- * Return: void
+ * jack_bauer - prints every minute of the day
+ *
+ * Return: no return
+ *
  */
 void jack_bauer(void)
 {
-int h1, h2, m1, m2;
-int a = 9;
-h2 = 0;
-while (h1 <= a)
-{
-if (h2 == 2)
-{
-a = 3;
-}
-h1 = 0;
-while (h1 <= a)
-{
-m2 = 0;
-while (m2 <= 5)
-{
-m1 = 0;
-while (m1 <= 9)
-{
-_putchar('0' + h2);
-_putchar('0' + h1);
-_putchar(':');
-_putchar('0' + m2);
-_putchar('0' + m1);
-_putchar('\n');
-m1++;
-}
-m2++;
-}
-h1++;
-}
-h2++;
-}
+	int a, b, c, d;
+
+	for (a = 48; a <= 50; a++)
+	{
+	for (b = 48; b <= 57; b++)
+	{
+	for (c = 48; c <= 53; c++)
+	{
+	for (d = 48; d <= 57; d++)
+	{
+	if (a >= 50 && b >= 52)
+	break;
+	_putchar(a);
+	_putchar(b);
+	_putchar(58);
+	_putchar(c);
+	_putchar(d);
+	_putchar('\n');
+	}
+	}
+	}
+	}
 }
